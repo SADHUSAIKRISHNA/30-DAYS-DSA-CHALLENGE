cd836533564c86502499846583bdba80f6921570
SADHUSAIKRISHNA <161282196+SADHUSAIKRISHNA@users.noreply.github.com>
2025-08-21 21:26:45 +0530

    Update day2-3.py

--- DAY2/day2-3.py
+++ DAY2/day2-3.py
@@ -7,12 +7,11 @@ class Solution(object):
         count = {}
         for char in s:
             count[char] = count.get(char, 0) + 1
-
-        
         for i in range(len(s)):
             if count[s[i]] == 1:
                 return i
         return -1  
 s = "sadhu sai"
 solution = Solution()
+
 print("Index of first unique character:", solution.firstUniqChar(s))
